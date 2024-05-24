d3ca534209a2284d98e7b53b444a5994b6c3e937
Usuario <Usuario@DESKTOP-F8UNUFP>
2024-05-24 17:21:27 -0600

    Agregar archivos de proyecto.

---
+++ Examen/Models/cExamen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class cExamen
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int IdExamen { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Descripcion { get; set; }
+
+    }
+}
